07ff510934e1f22a4938bd9864124b3a38e951bf
0x21q <kratochvil.kubik@gmail.com>
2025-03-08 14:16:22 +0100

    Adding basic functionality of task2

--- src/task2.hs
+++ src/task2.hs
@@ -6,7 +6,7 @@
 module TaskTwo (taskTwo) where
 
 import Common (DecisionTree (EmptyTree, Leaf, Node), replaceComma)
-import Data.List (minimumBy, sort, transpose)
+import Data.List (maximumBy, minimumBy, sort, transpose)
 import Data.Map qualified as Map
 import Data.Ord (comparing)
 
@@ -17,62 +17,51 @@ taskTwo :: String -> IO ()
 taskTwo trainPath = do
   contentsTrain <- readFile trainPath
   let allFeatCls = parseTrainData $ lines contentsTrain
-  let thrs = sort $ map fst $ head allFeatCls
-  let thrCs = thrCandidates $ map fst $ head allFeatCls
-  let bestThr = getBestThr thrCs allFeatCls
-  print thrs
-  print thrCs
-  print bestThr
-  let thrs2 = sort $ map fst $ last allFeatCls
-  let thrCs2 = thrCandidates $ map fst $ last allFeatCls
-  let bestThr2 = getBestThr thrCs2 allFeatCls
-  print thrs2
-  print thrCs2
-  print bestThr2
+  print $ trainTree allFeatCls 0
+
   let allSplits = getAllSplits allFeatCls
   let bestSplit = minimumBy (comparing (\(g, _, _, _) -> g)) allSplits
   print allSplits
+  putStrLn "\n"
   print bestSplit
-  -- print $ trainTree allFeatCls [0 .. length (head allFeat) - 1]
 
 parseTrainData :: [String] -> [[FeatCls]]
-parseTrainData lines = transpose $ createTuples allFeat allCls
-  where 
-    allCls = map (last . words . replaceComma) lines
-    allFeat = map (init . words . replaceComma) lines
+parseTrainData inputLines = transpose $ createTuples allFeat allCls
+  where
+    allCls = map (last . words . replaceComma) inputLines
+    allFeat = map (init . words . replaceComma) inputLines
 
 createTuples :: [[String]] -> [String] -> [[FeatCls]]
 createTuples [] _ = []
 createTuples _ [] = []
 createTuples (f : fs) (c : cs) = map (\x -> (read x, c)) f : createTuples fs cs
 
-trainTree :: [[FeatCls]] -> [Int] -> DecisionTree
-trainTree _ [] = EmptyTree
-trainTree allFeatCls [] = Leaf ((snd . head . head) allFeatCls)
-trainTree allFeatCls indices = Node bestIdx bestThr left right
+trainTree :: [[FeatCls]] -> Int -> DecisionTree
+trainTree [[]] _ = EmptyTree
+trainTree allFeatCls c = Node c bestThr left right
   where
     allSplits = getAllSplits allFeatCls
-    bestSplit@(_, bestThr, splitA, splitB) = minimumBy (comparing (\(g, _, _, _) -> g)) allSplits
-    splitPos = head [i | (i, split) <- zip [0 ..] allSplits, split == bestSplit]
-    bestIdx = indices !! splitPos
-    left = trainTree [splitA] (tail indices)
-    right = trainTree [splitB] (tail indices)
+    (gi, bestThr, splitA, splitB) = minimumBy (comparing (\(g, _, _, _) -> g)) allSplits
+    left =
+      if gi <= 0.3
+        then Leaf $ mostFrequentCls splitA
+        else trainTree [splitA] (c + 1)
+    right =
+      if gi <= 0.3
+        then Leaf $ mostFrequentCls splitB
+        else trainTree [splitB] (c + 1)
 
--- trainTree allFeatCls (i : is) = Node i thr left right
---  where
---    ((g, thr, splitA, splitB) : other) = getAllSplits allFeatCls
---    -- find the split that has the smallest gini and use that split to create
---    -- node and recursively build the left and right subtree
---    left = EmptyTree
---    right = EmptyTree
--- trainTree _ _ = EmptyTree
+mostFrequentCls :: [FeatCls] -> String
+mostFrequentCls [] = []
+mostFrequentCls xxs =
+  fst . maximumBy (comparing snd) . Map.toList $ classFreq (map snd xxs) Map.empty
 
 getAllSplits :: [[FeatCls]] -> [(Double, Double, [FeatCls], [FeatCls])]
 getAllSplits [] = []
 getAllSplits allFeatCls =
   (giniOfSplit, bestThr, splitA, splitB) : getAllSplits (tail allFeatCls)
   where
-    thrs = thrCandidates $ (sort . map fst . head) allFeatCls
+    thrs = fixFP . thrCandidates . sort . map fst . head $ allFeatCls
     bestThr = getBestThr thrs allFeatCls
     features = head allFeatCls
     splitA = filter (\f -> fst f <= bestThr) features -- alternatively as fold
@@ -81,12 +70,12 @@ getAllSplits allFeatCls =
 
 getBestThr :: [Double] -> [[FeatCls]] -> Double
 getBestThr [] _ = 1.0
-getBestThr xxs allFeatCls 
-    = fst $ minimumBy (comparing snd) $ map (\t -> (t, giniOfThr t)) xxs
+getBestThr xxs allFeatCls =
+  fst . minimumBy (comparing snd) . map (\t -> (t, giniOfThr t)) $ xxs
   where
     features = head allFeatCls
     giniOfThr thr = weightedGini splitA splitB
-      where 
+      where
         splitA = filter (\f -> fst f <= thr) features
         splitB = filter (\f -> fst f > thr) features
 
@@ -122,9 +111,9 @@ thrCandidates [] = []
 thrCandidates [_] = []
 thrCandidates (x : y : ys) = ((x + y) / 2) : thrCandidates (y : ys)
 
--- Return a threshold (avg) from a list of features
---thrAvg :: [(Double, String)] -> Double
---thrAvg [] = 0
---thrAvg xxs = (head feats + last feats) / 2
---  where
---    feats = sort $ map fst xxs
+fixFP :: [Double] -> [Double]
+fixFP [] = []
+fixFP (x : xs) =
+  if length (show x) > 4
+    then (read . take 4 . show) x : fixFP xs
+    else x : fixFP xs
