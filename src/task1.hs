c07763ef3f81a54051306ab2e938859b2ce421cf
0x21q <kratochvil.kubik@gmail.com>
2025-03-11 15:17:00 +0100

    Fixing classifyLine and adding small refactoring

--- src/task1.hs
+++ src/task1.hs
@@ -10,11 +10,10 @@ import Common (DecisionTree (EmptyTree, Leaf, Node), leadingSpaceCount, replaceC
 -- Execute the first task of the project
 taskOne :: String -> String -> IO ()
 taskOne treePath dataPath = do
-  contentsTree <- readFile treePath
-  let tree = buildTree $ lines contentsTree
-  -- print tree
-  contentsData <- readFile dataPath
-  mapM_ putStrLn $ classifyAll (lines contentsData) tree
+  treeInput <- readFile treePath
+  let tree = buildTree $ lines treeInput
+  dataInput <- readFile dataPath
+  mapM_ putStrLn $ classifyAll (lines dataInput) tree
 
 -- Build the tree from the lines in a file
 buildTree :: [String] -> DecisionTree
@@ -34,12 +33,13 @@ parseTree hCurr (line : other)
 generateNode :: Int -> [String] -> DecisionTree
 generateNode hCurr (line : other) =
   case words $ dropWhile (== ' ') line of
-    ("Node:" : idxS : thrS) ->
-      Node (read $ init idxS) (read $ head thrS) left right
+    ("Leaf:" : cls) -> Leaf $ head cls
+    ("Node:" : idxStr : thrStr) -> Node idx thr left right
       where
+        idx = read $ init idxStr
+        thr = read $ head thrStr
         left = parseTree (hCurr + 2) other
         right = parseTree (hCurr + 2) $ tail other
-    ("Leaf:" : cls) -> Leaf $ head cls
     _ -> EmptyTree
 generateNode _ _ = EmptyTree
 
@@ -48,11 +48,14 @@ classifyAll :: [String] -> DecisionTree -> [String]
 classifyAll [] _ = []
 classifyAll _ EmptyTree = []
 classifyAll (line : other) tree =
-  classifyLine (words $ replaceComma line) tree ++ classifyAll other tree
+  classifyLine (words $ replaceComma line) tree : classifyAll other tree
 
 -- Classify single line given from classifyAll
-classifyLine :: [String] -> DecisionTree -> [String]
-classifyLine _ (Leaf cls) = [cls]
-classifyLine (val : vals) (Node _ thr left right) =
-  if read val <= thr then classifyLine vals left else classifyLine vals right
-classifyLine _ _ = []
+classifyLine :: [String] -> DecisionTree -> String
+classifyLine [] _ = []
+classifyLine _ EmptyTree = []
+classifyLine _ (Leaf cls) = cls
+classifyLine vals (Node i thr left right) =
+  if read (vals !! i) <= thr
+    then classifyLine vals left
+    else classifyLine vals right
